6170ebf9e8f8ff9a773b5fcb9c7bc26214715730
Rasco101 <ms.elhosan@gmail.com>
2020-06-24 19:19:01 +0200

    SPControl Fixed

--- SPControl.vhd
+++ SPControl.vhd
@@ -17,25 +17,30 @@ architecture SPControl_imp of SPControl is
       SIGNAL next_empty: INTEGER;
 
 begin
-    process(en, dir) 
-	begin  
+    
+    process(en, dir, clk) 
+        VARIABLE index: INTEGER;
+    begin  
+        index := next_empty;
         if (rst = '1') THEN
+            index := (2 ** 11) - 1;	
 		    next_empty <= (2 ** 11) - 1;	
-            SPAddress <= std_logic_vector(to_unsigned(next_empty, 11) - 1);
-        else
-            if (en = '1') THEN
-                if (dir = '1')THEN
-                    IF (next_empty = 2047) THEN
-                        report "Stack is Empty Can't use POP" severity warning;
+            SPAddress <= std_logic_vector(to_unsigned(2 ** 11, 11) - 1);
+        ELSIF (rising_edge(clk)) THEN
+            IF (en = '1') THEN
+                IF (dir = '1') THEN
+                    if (index = 2047) THEN
+                        report "Stack Is Empty cannot use POP";
                     ELSE
-                        next_empty <= next_empty + 2;
-                        SPAddress <= std_logic_vector(to_unsigned(next_empty + 2, 11)) - 1;
+                        SPAddress <= std_logic_vector(to_unsigned(index, 11)) + 1;
+                        index := index + 2;
                     END IF;
-                else
-                    next_empty <= next_empty - 2;
-                    SPAddress <= std_logic_vector(to_unsigned(next_empty, 11)) - 1;
-                END if;
-            END if;
+                ELSE
+                    SPAddress <= std_logic_vector(to_unsigned(index, 11)) - 1;
+                    index := index - 2;
+                END IF;
+            END IF;       
         END if;
+        next_empty <= index;       
     END PROCESS;
 end SPControl_imp ; -- SPControl_imp
